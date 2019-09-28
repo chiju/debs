5317e3fb6ca6860fd9920cca2fd61fac2b88627f
chiju <chiju@devopsnexus.com>
2019-09-28 13:01:04 +0530

    ty

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-version=7.6
+version=7.7
 username='chiju'
 password='32ea322465698d89b0b2844be7d8007527516e6e'
+repository='debs'
+package='openssh'
+git_repo='https://github.com/chiju/debs.git'
+
 
 # creating version
-curl -svvf -u $username:$password -H "Content-Type: application/json" \
-                                  -X POST https://api.bintray.com/packages/chiju/debs/openssh/versions \
-                                  --data "{\"name\": \"$version\", \"github_use_tag_release_notes\": false }"
-git clone https://github.com/chiju/debs.git
+curl -svvf -u $username:$password \
+           -H "Content-Type: application/json" \
+           -X POST https://api.bintray.com/packages/$username/$repository/$package/versions \
+           --data "{\"name\": \"$version\", \"github_use_tag_release_notes\": false }"
+git clone git_repo
 cd debs
 
 #upoloading pacakges
@@ -19,8 +24,9 @@ do
 										-H "X-Bintray-Debian-Distribution:xenial" \
 										-H "X-Bintray-Debian-Component:main" \
 										-H "X-Bintray-Debian-Architecture:amd64" \
-										https://api.bintray.com/content/chiju/debs/openssh/$version/$f; 
+										https://api.bintray.com/content/$username/$repository/$package/$version/$f; 
 done
 
 # Publishing the version
-curl -vvf -u $username:$password -X POST https://api.bintray.com/content/chiju/debs/openssh/$version/publish
+curl -svvf -u $username:$password \
+           -X POST https://api.bintray.com/content/$username/$repository/$package/$version/publish
