565d7212ddbc166984e6fbd7718feb2e916242f6
chiju <chiju@devopsnexus.com>
2019-09-28 11:37:08 +0530

    j

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 version=40
-curl -vvf -u 'chiju:32ea322465698d89b0b2844be7d8007527516e6e' -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/chiju/debs/openssh/versions --data "{\"name\": \"$version\", \"github_use_tag_release_notes\": false }"
+curl -svvf -u 'chiju:32ea322465698d89b0b2844be7d8007527516e6e' -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/chiju/debs/openssh/versions --data "{\"name\": \"$version\", \"github_use_tag_release_notes\": false }"
 git clone https://github.com/chiju/debs.git
 cd debs
 for f in $(find -name "*.deb" -type f); 
 do 
 	echo "Uploading $f"; 
-	curl -vvf -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e \
+	curl -svvf -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e \
 										-H "X-Bintray-Version:$version" \
 										-H "X-Bintray-Package:openssh"  \
 										https://api.bintray.com/content/chiju/debs/openssh/$version/$f; 
