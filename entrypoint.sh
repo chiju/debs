aae7bb0262415e67cba8faed90ff04f30b6c6f1d
chiju <chiju@devopsnexus.com>
2019-09-28 11:25:23 +0530

    r

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-version=35
+version=38
 
 curl -vvf -u 'chiju:32ea322465698d89b0b2844be7d8007527516e6e' -H "Content-Type: application/json" -X POST https://api.bintray.com/packages/chiju/debs/openssh/versions --data "{\"name\": \"$version\", \"github_use_tag_release_notes\": false }"
 
 git clone https://github.com/chiju/debs.git
 
+cd debs
+
 ls -al
 
-for package in $(find -name "*.deb" -type f)
-do
+#for package in $(find -name "*.deb" -type f)
+#do
 	echo "Uploading";
-	curl -vvf  -X PUT -T $f -uchiju:32ea322465698d89b0b2844be7d8007527516e6e \
-												  -H "X-Bintray-Debian-Distribution:xenial" \
-												  -H "X-Bintray-Debian-Component:main" \
-												  -H "X-Bintray-Debian-Architecture:amd64" \
-												  https://api.bintray.com/content/chiju/debs/openssh/$version/$package;
-done
+	#curl -vvf  -X PUT -T $package -uchiju:32ea322465698d89b0b2844be7d8007527516e6e \
+	#											  			-H "X-Bintray-Debian-Distribution:xenial" \
+	#											  			-H "X-Bintray-Debian-Component:main" \
+	#											  			-H "X-Bintray-Debian-Architecture:amd64" \
+	#											  			https://api.bintray.com/content/chiju/debs/openssh/$version/$package;	
+#done
+
+for f in $(find -name "*.deb" -type f); do echo; echo "Uploading $f"; curl -X PUT -T $f -u chiju:32ea322465698d89b0b2844be7d8007527516e6e -H "X-Bintray-Version:$version" -H "X-Bintray-Package:openssh"  https://api.bintray.com/content/chiju/debs/openssh/$version/$f; done
